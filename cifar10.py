2499489d28e86d0912e0db5be1443c37a43b8cc5
leechongmyeong <lcm8728@gmail.com>
2018-02-19 20:23:18 +0900

    edit initial weight

--- cifar10.py
+++ cifar10.py
@@ -15,52 +15,47 @@ Y = tf.placeholder(tf.float32, [None, 10])
 #Y.shape=[None, 10]
 
 #L1
-W1 = tf.Variable(tf.random_normal([11, 11, 3, 96]), tf.float32)
-L1 = tf.nn.conv2d(X, W1, strides = [1, 1, 1, 1], padding = 'SAME')
-L1 = tf.nn.relu(L1)
-L1 = tf.nn.max_pool(L1, ksize=[1, 2, 2, 1], strides = [1, 2, 2, 1], padding='SAME')
-#L1.shape=[None, 16, 16, 96] 
+w1 = tf.Variable(tf.random_normal([8, 8, 3, 32], stddev=0.01))
+l1 = tf.nn.relu(tf.nn.conv2d(X, w1, strides = [1, 1, 1, 1], padding = 'SAME'))
+l1_max_pool = tf.nn.max_pool(l1, ksize=[1, 2, 2, 1], strides = [1, 2, 2, 1], padding='SAME')
+#L1.shape=[None, 16, 16, 32] 
 
 #L2
-W2 = tf.Variable(tf.random_normal([5, 5, 96, 256]), tf.float32)
-L2 = tf.nn.conv2d(L1, W2, strides = [1, 1, 1, 1], padding = 'SAME')
-L2 = tf.nn.relu(L2)
-L2 = tf.nn.max_pool(L2, ksize=[1, 2, 2, 1], strides = [1, 2, 2, 1], padding='SAME')
-#L2.shape=[None, 8, 8, 256]
+w2 = tf.Variable(tf.random_normal([5, 5, 32, 64], stddev=0.01)) 
+l2 = tf.nn.relu(tf.nn.conv2d(l1_max_pool, w2, strides = [1, 1, 1, 1], padding = 'SAME'))
+l2_max_pool = tf.nn.max_pool(l2, ksize=[1, 2, 2, 1], strides = [1, 2, 2, 1], padding='SAME')
+#L2.shape=[None, 8, 8, 64]
 
 #L3
-W3 = tf.Variable(tf.random_normal([3, 3, 256, 384]), tf.float32)
-L3 = tf.nn.conv2d(L2, W3, strides = [1, 1, 1, 1], padding = 'SAME')
-L3 = tf.nn.relu(L3)
-#L3.shape=[None, 8, 8, 384] 
+w3 = tf.Variable(tf.random_normal([3, 3, 64, 128], stddev=0.01))
+l3 = tf.nn.relu(tf.nn.conv2d(l2_max_pool, w3, strides = [1, 1, 1, 1], padding = 'SAME'))
+#L3.shape=[None, 8, 8, 128] 
 
 #L4
-W4 = tf.Variable(tf.random_normal([3, 3, 384, 384]), tf.float32)
-L4 = tf.nn.conv2d(L3, W4, strides = [1, 1, 1, 1], padding = 'SAME')
-L4 = tf.nn.relu(L4)
-L4 = tf.nn.max_pool(L4, ksize=[1, 2, 2, 1], strides = [1, 2, 2, 1], padding='SAME')
-#L4.shape=[None, 4, 4, 384]
+w4 = tf.Variable(tf.random_normal([3, 3, 128, 128], stddev=0.01))
+l4 = tf.nn.relu(tf.nn.conv2d(l3, w4, strides = [1, 1, 1, 1], padding = 'SAME'))
+#L4.shape=[None, 8, 8, 128]
 
 dropout_rate = tf.placeholder(tf.float32)
 
 #FC1
-L4 = tf.reshape(L4, [-1, 4*4*384])
-W5 = tf.get_variable("W5", [4*4*384, 384], initializer=tf.contrib.layers.xavier_initializer())
-b5 = tf.Variable(tf.zeros([384]), tf.float32)
-L5 = tf.nn.relu(tf.matmul(L4, W5) + b5)
-L5_drop = tf.nn.dropout(L5, dropout_rate)
+l4_flat = tf.reshape(l4, [-1, 8*8*128])
+w5 = tf.get_variable("w5", [8*8*128, 128], initializer=tf.contrib.layers.xavier_initializer())
+b5 = tf.Variable(tf.constant(0., shape=[128]))
+l5 = tf.nn.relu(tf.matmul(l4_flat, w5) + b5)
+l5_drop = tf.nn.dropout(l5, dropout_rate)
 
 #FC2
-W6 = tf.get_variable("W6", [384, 384], initializer=tf.contrib.layers.xavier_initializer())
-b6 = tf.Variable(tf.zeros([384]), tf.float32)
-L6 = tf.nn.relu(tf.matmul(L5_drop,W6) + b6)
-L6_drop = tf.nn.dropout(L6, dropout_rate)
+w6 = tf.get_variable("w6", [128, 384], initializer=tf.contrib.layers.xavier_initializer())
+b6 = tf.Variable(tf.constant(0., shape=[384]))
+l6 = tf.nn.relu(tf.matmul(l5_drop,w6) + b6)
+l6_drop = tf.nn.dropout(l6, dropout_rate)
 
 #FC3
-W7 = tf.get_variable("W7", [384, 10], initializer=tf.contrib.layers.xavier_initializer())
-b7 = tf.Variable(tf.zeros([10]), tf.float32)
+w7 = tf.get_variable("w7", [384, 10], initializer=tf.contrib.layers.xavier_initializer())
+b7 = tf.Variable(tf.constant(0., shape=[10]))
 
-hypothesis = tf.matmul(L6_drop,W7) + b7
+hypothesis = tf.matmul(l6_drop, w7) + b7
 loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=hypothesis, labels=Y))
 
 optimizer = tf.train.AdamOptimizer(learning_rate = 0.001).minimize(loss)
@@ -89,17 +84,17 @@ with tf.Session() as sess:
         for step in range(train_size/batch_size):
             
             batch_xs, batch_ys = next_batch(step, batch_size, x_train, y_train_one_hot.eval())
-            h, l, _ = sess.run([accuracy, loss, optimizer], feed_dict={X:batch_xs, Y:batch_ys, dropout_rate:1.0})
-            print 'step : ', step, 'accuracy : ', h
+            h, l, _ = sess.run([accuracy, loss, optimizer], feed_dict={X:batch_xs, Y:batch_ys, dropout_rate:0.7})
+            print 'step : ', step,  'cost : ', l, 'accuracy : ', h
 
-        print 'epoch : %04d' %(epoch+1), 'cost : ', l, 'accuracy : ', accuracy.eval(feed_dict={X:batch_xs, Y:batch_ys, dropout_rate:0.7})
+        print 'epoch : %04d' %(epoch+1), 'cost : ', l, 'accuracy : ', accuracy.eval(feed_dict={X:batch_xs, Y:batch_ys, dropout_rate:1})
 
     
     #test
     f_a = 0.
     for i in range(10):
         batch_xs, batch_ys = next_batch(i, 1000, x_test, y_test_one_hot.eval())
-        a = accuracy.eval(feed_dict={X:batch_xs, Y:batch_ys, dropout:1.0})
+        a = accuracy.eval(feed_dict={X:batch_xs, Y:batch_ys, dropout_rate:1.0})
         f_a += a
         print 'accuracy : ', a
 
