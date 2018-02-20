f4beecbe0462415d8e39701fbdcf1c9708dae88c
leechongmyeong <lcm8728@gmail.com>
2018-02-20 15:27:04 +0900

    add more layer, adjust conv1 filter size(8 -> 7)

--- cifar10.py
+++ cifar10.py
@@ -12,14 +12,15 @@ labels = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse
 X = tf.placeholder(tf.float32, [None, 32, 32, 3])
 Y = tf.placeholder(tf.float32, [None, 10])
 
-#L1
-w1 = tf.Variable(tf.random_normal([8, 8, 3, 32], stddev=0.01))
+#l1
+w1 = tf.Variable(tf.random_normal([5, 5, 3, 32], stddev=0.01))
 l1 = tf.nn.relu(tf.nn.conv2d(X, w1, strides = [1, 1, 1, 1], padding = 'SAME'))
+#l1_max_pool
 l1_max_pool = tf.nn.max_pool(l1, ksize=[1, 2, 2, 1], strides = [1, 2, 2, 1], padding='SAME')
 #[None, 16, 16, 32] 
 
-#L2
-w2 = tf.Variable(tf.random_normal([5, 5, 32, 64], stddev=0.01)) 
+#l2
+w2 = tf.Variable(tf.random_normal([3, 3, 32, 64], stddev=0.01)) 
 l2 = tf.nn.relu(tf.nn.conv2d(l1_max_pool, w2, strides = [1, 1, 1, 1], padding = 'SAME'))
 l2_max_pool = tf.nn.max_pool(l2, ksize=[1, 2, 2, 1], strides = [1, 2, 2, 1], padding='SAME')
 #[None, 8, 8, 64]
@@ -30,33 +31,42 @@ l3 = tf.nn.relu(tf.nn.conv2d(l2_max_pool, w3, strides = [1, 1, 1, 1], padding = 
 #[None, 8, 8, 128] 
 
 #L4
-w4 = tf.Variable(tf.random_normal([3, 3, 128, 128], stddev=0.01))
+w4 = tf.Variable(tf.random_normal([3, 3, 128, 256], stddev=0.01))
 l4 = tf.nn.relu(tf.nn.conv2d(l3, w4, strides = [1, 1, 1, 1], padding = 'SAME'))
-#[None, 8, 8, 128]
+#[None, 8, 8, 256]
+
+#l5
+w5 = tf.Variable(tf.random_normal([3, 3, 256, 512], stddev=0.01))
+l5 = tf.nn.relu(tf.nn.conv2d(l4, w5, strides=[1, 1, 1, 1], padding = 'SAME'))
+
+#l6
+w6 = tf.Variable(tf.random_normal([3, 3, 512, 600], stddev=0.01))
+l6 = tf.nn.relu(tf.nn.conv2d(l5, w6, strides=[1, 1, 1, 1], padding = 'SAME'))
+l6_max_pool = tf.nn.max_pool(l6, ksize = [1, 2, 2, 1], strides = [1, 2, 2, 1], padding = 'SAME')
 
 dropout_rate = tf.placeholder(tf.float32)
 
-#FC1
-l4_flat = tf.reshape(l4, [-1, 8*8*128])
-w5 = tf.get_variable("w5", [8*8*128, 128], initializer=tf.contrib.layers.xavier_initializer())
-b5 = tf.Variable(tf.constant(0., shape=[128]))
-l5 = tf.nn.relu(tf.matmul(l4_flat, w5) + b5)
-l5_drop = tf.nn.dropout(l5, dropout_rate)
-#[None, 128]
+#fc1
+l6_flat = tf.reshape(l6_max_pool, [-1, 4*4*600])
+w7 = tf.get_variable("w7", [4*4*600, 256], initializer=tf.contrib.layers.xavier_initializer())
+b7 = tf.Variable(tf.constant(0., shape=[256]))
+l7 = tf.nn.relu(tf.matmul(l6_flat, w7) + b7)
+l7_drop = tf.nn.dropout(l7, dropout_rate)
+#[None, 256]
 
-#FC2
-w6 = tf.get_variable("w6", [128, 384], initializer=tf.contrib.layers.xavier_initializer())
-b6 = tf.Variable(tf.constant(0., shape=[384]))
-l6 = tf.nn.relu(tf.matmul(l5_drop,w6) + b6)
-l6_drop = tf.nn.dropout(l6, dropout_rate)
+#fc2
+w8 = tf.get_variable("w8", [256, 128], initializer=tf.contrib.layers.xavier_initializer())
+b8 = tf.Variable(tf.constant(0., shape=[128]))
+l8 = tf.nn.relu(tf.matmul(l7_drop, w8) + b8)
+l8_drop = tf.nn.dropout(l8, dropout_rate)
 #[None, 384]
 
-#FC3
-w7 = tf.get_variable("w7", [384, 10], initializer=tf.contrib.layers.xavier_initializer())
-b7 = tf.Variable(tf.constant(0., shape=[10]))
+#fc3
+w9 = tf.get_variable("w9", [128, 10], initializer=tf.contrib.layers.xavier_initializer())
+b9 = tf.Variable(tf.constant(0., shape=[10]))
 #[None, 10]
 
-hypothesis = tf.matmul(l6_drop, w7) + b7
+hypothesis = tf.matmul(l8_drop, w9) + b9
 loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=hypothesis, labels=Y))
 
 optimizer = tf.train.AdamOptimizer(learning_rate = 0.001).minimize(loss)
